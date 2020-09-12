a00381fa0542045f8a87cdfde0a544247e79ede5
Phobacloh <anna.lowery@hotmail.com>
2020-09-12 12:50:43 +0800

    added complete and remove functionality

--- src/App.js
+++ src/App.js
@@ -7,18 +7,29 @@ function App() {
 
   // variables
   const [todos, setTodos] = useState([
-    "Finish plus project",
-    "Learn to breakdance",
-    "Backflip",
+    {text:"Finish plus project", isCompleted: false},
+    {text:"Learn to breakdance", isCompleted: false},
+    {text:"Backflip", isCompleted: false},
   ]);
 
 // methods
 
   const addTodo = (text) => {
-    const newtodos = [...todos, text];
-    setTodos(newtodos);
+    const newTodos = [...todos, {text}];
+    setTodos(newTodos);
   }
 
+  const completeToDo = (index) => {
+    const newTodos = [...todos];
+    newTodos[index].isCompleted = true
+    setTodos(newTodos);
+  }
+
+  const removeToDo = (index) => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+}
 // template
   return (
   <div className="app">
@@ -26,18 +37,18 @@ function App() {
       <div>
         <h1>My todo list</h1>
         {todos.map((todo, index) => (
-          <TodoItem todo={todo} key={index} />
+          <TodoItem todo={todo} 
+          key={index} 
+          index={index} 
+          completeToDo={completeToDo}
+          removeToDo={removeToDo} 
+          />
         ))} 
         <TodoForm addTodo={addTodo} />
       </div>
     </div>
   </div>
 );
-}
+        }
 
-export default App;
-
-// have items
-// remove items
-// add items
-
+export default App
--- src/components/TodoItem/ToDoItem.jsx
+++ src/components/TodoItem/ToDoItem.jsx
@@ -2,8 +2,16 @@ import React from "react"
 import "./TodoItem.css"
 
 function TodoItem(props) {
-    const { todo } = props
-    return <div className="todo">{ todo }</div>;
+    const { todo, index, completeToDo, removeToDo } = props;
+    return (
+    <div className={`todo ${todo.isCompleted ? "complete" : ""}`}>
+    { todo.text }
+        <div>
+            <button onClick={() => completeToDo(index)}>Complete</button>
+            <button onClick={() => removeToDo(index)}>x</button>
+        </div>
+    </div>
+    );
 }
 
 export default TodoItem
