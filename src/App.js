d917676a138df18275de295b3636d569ccc13a39
Phobacloh <anna.lowery@hotmail.com>
2020-09-12 11:16:47 +0800

    Added items todo component. Can be Props allows it to render different text for the same variable "todo"

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import TodoItem from "./components/ToDoItem";
+
+function App() {
+    return (
+    <div>
+      <h1>My todo list</h1>
+      <TodoItem todo="Finish Plus project"/> 
+      <TodoItem todo="Hair cut"/> 
+      <TodoItem todo="Learn to break dance"/> 
+      <TodoItem todo="Backflip"/>    
+    </div>
+  );
+}
+
+export default App;
+
+// have items
+// remove items
+// add items
+
